77477ca77d794ad89d0b1790cc3f8d1c7556080f
N. Minaev <minaev.36.ru@gmail.com>
2022-03-05 11:02:32 +0300

    Промежуточная версия

--- parse.go
+++ parse.go
@@ -11,6 +11,15 @@ import (
 	_ "text/template"
 )
 
+// MobaXTerm connection struct
+type MobaXTerm struct {
+	RecordID      string // Bookmark #
+	RKName        string // Региональная компания
+	AptName       string // Имя аптеки в нотации типа 'мскАпт1001'
+	ServerAddress string //непостоянная часть адреса сервера
+	Username      string // пользователь системы, efarma по умолчанию
+}
+
 func main() {
 	print("Hello World!")
 }
