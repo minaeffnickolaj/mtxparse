b54de0c448380698ce63145fc8f351b93d82fd9d
N. Minaev <minaev.36.ru@gmail.com>
2022-03-05 20:41:24 +0300

    Почти готовая версия, планируется добавить вывод в ФАЙЛ и параметризация путей

--- parse.go
+++ parse.go
@@ -8,12 +8,15 @@ package main
 */
 
 import (
-	_ "text/template"
+	"os"
+	"text/template"
+
+	"github.com/xuri/excelize/v2"
 )
 
 // MobaXTerm connection struct
 type MobaXTerm struct {
-	RecordID      string // Bookmark #
+	APCode        string
 	RKName        string // Региональная компания
 	AptName       string // Имя аптеки в нотации типа 'мскАпт1001'
 	ServerAddress string // Непостоянная часть адреса сервера
@@ -22,15 +25,45 @@ type MobaXTerm struct {
 
 func main() {
 	print("Let's go parse files...")
-
-	//creating output file
-	/*
-		MXTFile, err := os.Create("output.mxtsessions")
-		if err != nil {
-			println("Unable to create output file:", err)
-			os.Exit(1)
+	//Opening XLSX file
+	XLSXFile, err := excelize.OpenFile("apt.xlsx")
+	if err != nil {
+		println("Unable to load XLSX file:", err)
+	}
+	defer func() { // correctly close XLSX
+		if err := XLSXFile.Close(); err != nil {
+			println(err)
+			println("Parsing done!")
 		}
-		defer MXTFile.Close()
-	*/
-
+	}()
+	// struct slice of MobaXTerm connections
+	MXTConnectionsSlice := []MobaXTerm{}
+	//read XLSX
+	Rows, err := XLSXFile.GetRows("Аптеки")
+	if err != nil {
+		println(err)
+		return
+	}
+	for _, row := range Rows {
+		/*
+			Excel row in Excelize realized like [][]string, when each
+			cell is [i][y] - i is Row, y is Cell
+		*/
+		MXTConnection := new(MobaXTerm) //create MXT
+		MXTConnection.APCode = row[1]
+		MXTConnection.RKName = row[2]
+		MXTConnection.AptName = row[0]
+		MXTConnection.ServerAddress = row[3]
+		MXTConnection.Username = "efarma"
+		MXTConnectionsSlice = append(MXTConnectionsSlice, *MXTConnection) // pointer to MXTConnection
+	}
+	//Begin generate MXT file
+	//Creating template of MobaXTerm connection
+	MXTTemplate := template.New("MobaXTermTemplate")
+	MXTTemplateText := "\n\n[Bookmarks_2]\nSubRep={{.RKName}}\\{{.AptName}}\nImgNum=41\n{{.AptName}}({{.APCode}})=#91#4%{{.ServerAddress}}.apt.rigla.ru%10433%[{{.Username}}]%0%-1%-1%-1%-1%0%0%-1%%%%%0%0%%-1%%-1%-1%0%-1%0%-1#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%-1%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1#0# #-1"
+	MXTTemplate.Parse(MXTTemplateText)
+	for _, MobaXTerm := range MXTConnectionsSlice {
+		MXTTemplate.Execute(os.Stdout, MobaXTerm)
+	}
+	println("END")
 }
