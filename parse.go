2fc35d98d25c8820bec1a6e465cf2b6f66d832d0
N. Minaev <minaev.36.ru@gmail.com>
2022-03-06 05:02:53 +0300

    Рабочая версия

--- parse.go
+++ parse.go
@@ -8,10 +8,14 @@ package main
 */
 
 import (
+	"bufio"
+	"bytes"
+	"io/ioutil"
 	"os"
 	"text/template"
 
 	"github.com/xuri/excelize/v2"
+	"golang.org/x/text/encoding/charmap"
 )
 
 // MobaXTerm connection struct
@@ -62,8 +66,45 @@ func main() {
 	MXTTemplate := template.New("MobaXTermTemplate")
 	MXTTemplateText := "\n\n[Bookmarks_2]\nSubRep={{.RKName}}\\{{.AptName}}\nImgNum=41\n{{.AptName}}({{.APCode}})=#91#4%{{.ServerAddress}}.apt.rigla.ru%10433%[{{.Username}}]%0%-1%-1%-1%-1%0%0%-1%%%%%0%0%%-1%%-1%-1%0%-1%0%-1#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%-1%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1#0# #-1"
 	MXTTemplate.Parse(MXTTemplateText)
-	for _, MobaXTerm := range MXTConnectionsSlice {
-		MXTTemplate.Execute(os.Stdout, MobaXTerm)
+	//create file to save output
+	ParsedFile, err := os.Create("unprepared.mxtconnections")
+	if err != nil {
+		println(err)
+		println("File not created!")
+		return
 	}
-	println("END")
+	for _, MobaXTerm := range MXTConnectionsSlice {
+		MXTTemplate.Execute(ParsedFile, MobaXTerm)
+	}
+	// finally
+	ConvertFileToWin1251(FileToString())
+	println("Job done!")
+}
+
+func FileToString() string {
+	File, err := os.Open("unprepared.mxtconnections") //open file
+	if err != nil {
+		println("Error in FileToString func!", err)
+	}
+	ReturnedString := bufio.NewScanner(File) //scan file
+	var BufferString bytes.Buffer
+	StringWriter := bufio.NewWriter(&BufferString) // write to []byte
+	for ReturnedString.Scan() {
+		StringWriter.Write(ReturnedString.Bytes())
+		StringWriter.WriteByte('\n')
+	}
+	if !ReturnedString.Scan() {
+		println("Failed to read: %v", ReturnedString.Err())
+	}
+	Result := string(BufferString.Bytes())
+	return Result
+}
+
+func ConvertFileToWin1251(InputString string) {
+	Encoder := charmap.Windows1251.NewEncoder()
+	s, err := Encoder.String(InputString)
+	if err != nil {
+		panic(err)
+	}
+	ioutil.WriteFile("output.mxtsessions", []byte(s), os.ModePerm)
 }
